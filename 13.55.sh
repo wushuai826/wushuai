2ac0c69cba3cd7ec79563b89e01685858f5a9ab4
wushuai <swu@shaklee.com>
2017-10-19 15:03:44 +0800

    9X9.sh

--- 13.55.sh
+++ 13.55.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 i=9
-while [ $j -ge 1 ];do
+while [ $i -ge 1 ];do
+    j=1
+    while [ $j -le 9 ];do
+        echo -n " $j*$i=$(($j*$i)) "
+	    if [ $j == $i ];then
+                echo -e "\n"
+		break
+	    fi
+        let j++
+    done
+    let i--
+done
 
